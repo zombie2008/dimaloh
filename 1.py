d3de08a35f9e414718bc01ebdf979dd91b9e86a9
zombie2008 <101250020+zombie2008@users.noreply.github.com>
2022-04-17 22:07:30 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,18 @@
+﻿from time import sleep
+list = ['один', 'двa', 'три', 'четыре', 'пять']
+input = open('data.txt')
+output = open('dataRu.txt', 'w')
+k = 0
+index = 0
+s = ''
+for i in input:
+	for j in i:
+		if j != ' ':
+			k+=1
+		else:
+			break
+	output.write(list[index] + i[k:])
+	k=0
+	index+=1
+input.close()
+output.close()
